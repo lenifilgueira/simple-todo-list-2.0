2e371438b9ca54bc3dc2f6848433d07077ddaea3
lenifilgueira <64507172+lenifilgueira@users.noreply.github.com>
2020-05-05 18:28:50 -0400

    Updates

---
+++ precache-manifest.d3102406ab67022f57a43bd153036f81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3beea8f59cb61002dd5969c6fe3bbb30",
+    "url": "/simple-todo-list-2.0/index.html"
+  },
+  {
+    "revision": "52a7c13bce7c96fa3d7c",
+    "url": "/simple-todo-list-2.0/static/css/main.137d0d62.chunk.css"
+  },
+  {
+    "revision": "b71aa7611937b7030691",
+    "url": "/simple-todo-list-2.0/static/js/2.05ec1414.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-list-2.0/static/js/2.05ec1414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52a7c13bce7c96fa3d7c",
+    "url": "/simple-todo-list-2.0/static/js/main.02fb186c.chunk.js"
+  },
+  {
+    "revision": "151e36f4d5b238d8e646",
+    "url": "/simple-todo-list-2.0/static/js/runtime-main.acdb39b6.js"
+  }
+]);
